2985f961517af41753cb43953f646ac0f385fc2b
Shalabh Agarwal <sagarwal10@hotmail.com>
2021-08-04 15:20:20 -0700

    added navbar

--- movietable/src/Navigation.js
+++ movietable/src/Navigation.js
@@ -20,4 +20,23 @@ const Navigation = () => {
 	  </Nav.Link>
 	  <ModalLogin show={modelShow} onHide={()=>setModalShow(false)} />
 	  <Nav.Link href="#link">Link</Nav.Link>
+	  <NavDropdown title="Sample Dropdown" id="basic-nav-dropdown">
+	    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+	    <NavDropdown.Item href="#action/3.2">Another Action</NavDropdown.Item>
+	    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+	    <NavDropdown.Divider />
+	    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+	  </NavDropdown>
+        </Nav>
+	<Form inline>
+	  <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+	  <Button variant="outline-light">Search</Button>
+	</Form>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Navigation;
+
 
