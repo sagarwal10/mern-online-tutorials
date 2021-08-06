8ad3e8eb1c5a55b0ca6eb8fee15912f4720faf6b
Shalabh Agarwal <sagarwal10@hotmail.com>
2021-08-06 11:58:57 -0700

    Added TMDB axios fetch

--- movietable/src/App.js
+++ movietable/src/App.js
@@ -6,6 +6,43 @@ import Navigation from 'Navigation';
 import MovieTable from 'MovieTable';
 import MovieAccordion from 'MoveAccordion';
 
+const [movies, setMovies] = useState([]);
+const [err, setErr] = useState("");
+const [errBool, setErrBool] = useState(false);
+const [loading, setLoading] = useState(true); 
+
+let URL = https://api.themoviedb.org/3/movie/550?api_key=df632ef0878241f1bb9d9f851a1f5bde;
+
+const fetchMovies = (url) => {
+  axios.get(url)
+       .then(response => {
+         setMovies(response.data.results);
+	 setLoading(false);
+       })
+       .catch(err => {
+         setErr(err.message);
+	 setErrBool(true);
+	 setLoading(false);
+       });
+} 
+
+const fetchMovies = (url) => {
+ axios.get(url)
+      .then(response => {
+        setMovies(response.data.results);
+        setLoading(false);
+      })
+      .catch(err => {
+	setErr(err.message);
+	setErrBool(true);
+	setLoading(false);
+      })
+}
+
+useEffect(() => {
+  fetchMovies(URL);
+}, [URL]);
+
 function App() {
   return (
 <Container>
